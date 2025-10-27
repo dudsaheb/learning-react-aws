8e26e4fcb5596c0ba9048942df0bb4a395622aeb
dudsaheb <dudsaheb@gmail.com>
2025-10-27 20:05:57 +0530

    first commit 15

--- src/App.jsx
+++ src/App.jsx
@@ -23,6 +23,7 @@ function App() {
           <Routes>
             {/* <Route path="/" element={<LoginPage />} /> */}
             <Route path="/" element={<WelcomePage />} />
+            <Route path="/welcome" element={<WelcomePage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/register" element={<RegisterPage />} />
             <Route
